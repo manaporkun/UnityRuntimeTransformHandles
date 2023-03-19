8aa7964316755aade75c74dfbb7ab2d44a9b7393
Orkun Manap <31966136+manaporkun@users.noreply.github.com>
2023-03-20 00:35:03 +0300

    New scripts

---
+++ Assets/Scripts/TransformHandle/HandleCameraEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace TransformHandle
+{
+    public class HandleCameraEventHandler : MonoBehaviour
+    {
+        public event Action PreRender;
+        
+        private void OnPreRender()
+        {
+            PreRender?.Invoke();
+        }
+    }
+}
