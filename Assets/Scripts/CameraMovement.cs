459069316ce295405f77dd93ddd94fc8d85d4a9b
Orkun Manap <manaporkun@protonmail.com>
2023-03-26 16:28:19 +0300

    Small change

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [Range(0.1f, 9f)] [SerializeField] private float sensitivity = 2f;
+    [Range(0f, 9f)] [SerializeField] private float sensitivity = 2f;
     [Range(0f, 90f)] [SerializeField] private float yRotationLimit = 60f;
     [Range(0f, 90f)] [SerializeField] private float xRotationLimit = 60f;
     [SerializeField] private float zoomSpeed = 10f;
@@ -28,6 +28,7 @@ public class CameraMovement : MonoBehaviour
 
     private void UpdateRotation()
     {
+        if(sensitivity == 0f) return;
         _rotation.x += Input.GetAxis(XAxis) * sensitivity;
         _rotation.x = Mathf.Clamp(_rotation.x, -xRotationLimit, xRotationLimit);
 
@@ -42,6 +43,7 @@ public class CameraMovement : MonoBehaviour
 
     private void UpdateCameraZoom()
     {
+        if(zoomSpeed == 0f) return;
         _fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
         _camera.fieldOfView = _fieldOfView;
         _camera.fieldOfView = Mathf.Clamp(_fieldOfView, 35f, 100f);
